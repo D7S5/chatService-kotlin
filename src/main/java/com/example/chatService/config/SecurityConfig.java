49c187a432c182de6ec9e1b762c8b45f28c0bd5f
austin <doublehoon2@gmail.com>
2026-04-07 12:49:46 +0900

    k8s

--- src/main/java/com/example/chatService/config/SecurityConfig.java
+++ src/main/java/com/example/chatService/config/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://172.28.4.215:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
